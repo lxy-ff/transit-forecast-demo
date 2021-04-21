9acd27d78a24f3813454bfb60f6b05916435db6a
lxy-ff <77622072+lxy-ff@users.noreply.github.com>
2021-04-21 18:05:02 +0800

    Add files via upload

---
+++ py/fwwb_a09/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for fwwb_a09 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fwwb_a09.settings')
+
+application = get_wsgi_application()
